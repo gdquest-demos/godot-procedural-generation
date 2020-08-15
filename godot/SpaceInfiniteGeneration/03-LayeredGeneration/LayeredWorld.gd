925d647ce493c91c6a4cbdc0a05bfc75f8837ef1
Francois Belair <razoric480@gmail.com>
2020-08-15 16:32:47 -0400

    [Infinite gen] Fix planet algorithm and generate initial

--- godot/SpaceInfiniteGeneration/03-LayeredGeneration/LayeredWorld.gd
+++ godot/SpaceInfiniteGeneration/03-LayeredGeneration/LayeredWorld.gd
@@ -1,7 +1,7 @@
 extends BlueNoiseWorldGenerator
 
 
-export var planet_formation_threshold := 10000.0
+export var planet_formation_threshold := 10100.0
 export var Planet: PackedScene
 
 
@@ -45,31 +45,31 @@ func _update_sector(difference: Vector2) -> void:
 
 
 func _planet_generate_at(_x_id: int, _y_id: int) -> void:
-	rng.seed = make_seed_for(_x_id, _y_id, "planets")
-	
-	var sector_position := Vector2(_x_id, _y_id) * sector_size
+	var current_key := Vector2(_x_id, _y_id)
+	var sector_position := current_key * sector_size
 	var max_distance := sqrt(sector_size_square)
-	
+
 	var asteroid_weight := 0.0
-	
+
 	for x in range(-1, 2):
 		for y in range(-1, 2):
 			if x == 0 and y == 0:
 				continue
-			
-			var key := Vector2(_x_id + x, _y_id + y)
+
+			var key := current_key + Vector2(x, y)
 			var sector_data: Array = sectors[key]
 			
 			for asteroid in sector_data:
 				if asteroid.is_in_group("asteroids"):
 					var distance_weight: float = sector_position.distance_to(asteroid.global_position) * asteroid.scale.x
 					asteroid_weight += distance_weight
-				if asteroid.is_in_group("planets"):
-					return
-	
+
 	if asteroid_weight > planet_formation_threshold:
+		rng.seed = make_seed_for(_x_id, _y_id, "planets")
+		
 		var planet := Planet.instance()
 		add_child(planet)
-		planet.global_position = sector_position
-		sectors[Vector2(_x_id, _y_id)].append(planet)
+		sectors[current_key].append(planet)
 		planet.setup(rng)
+		planet.global_position = sector_position + Vector2(rng.randf_range(-sector_size/4, sector_size/4), rng.randf_range(-sector_size/4, sector_size/4))
+		planet.scale *= rng.randf_range(0.5, 1.5)
