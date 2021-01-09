6070b3c7a63c6912bc330a68ae3ece432f08a358
Nathan Lovato <nathan@gdquest.com>
2021-01-09 14:00:19 -0600

    Rename variables in BlueNoiseWorld.gd

--- godot/SpaceInfiniteGeneration/02-BlueNoiseWorld/BlueNoiseWorld.gd
+++ godot/SpaceInfiniteGeneration/02-BlueNoiseWorld/BlueNoiseWorld.gd
@@ -15,16 +15,16 @@ export var asteroid_density := 3
 export var sector_margin_proportion := 0.1
 
 ## The percentage of any given subsector to pad their margins by.
-export var sub_sector_margin_proportion := 0.1
+export var subsector_margin_proportion := 0.1
 
 # We generate a sub-grid based on the closest square number that is greater than `asteroid_density`.
-onready var _sub_sector_grid_width: int = ceil(sqrt(asteroid_density))
-onready var _sub_sector_count := _sub_sector_grid_width * _sub_sector_grid_width
+onready var _subsector_grid_width: int = ceil(sqrt(asteroid_density))
+onready var _subsector_count := _subsector_grid_width * _subsector_grid_width
 
 onready var _sector_margin := sector_size * sector_margin_proportion
-onready var _sub_sector_base_size := (sector_size - _sector_margin * 2) / _sub_sector_grid_width
-onready var _sub_sector_margin := _sub_sector_base_size * sub_sector_margin_proportion
-onready var _sub_sector_size := _sub_sector_base_size - _sub_sector_margin * 2
+onready var _subsector_base_size := (sector_size - _sector_margin * 2) / _subsector_grid_width
+onready var _subsector_margin := _subsector_base_size * subsector_margin_proportion
+onready var _subsector_size := _subsector_base_size - _subsector_margin * 2
 
 onready var _grid_drawer := $GridDrawer
 onready var _player := $Player
@@ -71,13 +71,13 @@ func _generate_sector(x_id: int, y_id: int) -> void:
 	var sector_data := []
 	# We generate as many indices as there are sub-sectors and shuffle the
 	# numbers.
-	var sector_indices = range(_sub_sector_count)
+	var sector_indices = range(_subsector_count)
 	sector_indices.shuffle()
 
 	for i in range(asteroid_density):
 		# Calculate the sub-sector coordinates for this asteroid.
-		var x := int(sector_indices[i] / _sub_sector_grid_width)
-		var y: int = sector_indices[i] - x * _sub_sector_grid_width
+		var x := int(sector_indices[i] / _subsector_grid_width)
+		var y: int = sector_indices[i] - x * _subsector_grid_width
 
 		# Generates a new asteroid inside of that sub-sector boundary.
 		var asteroid := Asteroid.instance()
@@ -90,12 +90,15 @@ func _generate_sector(x_id: int, y_id: int) -> void:
 	_sectors[Vector2(x_id, y_id)] = sector_data
 
 
-func _generate_random_position(sub_sector_coordinates: Vector2, sector_top_left: Vector2) -> Vector2:
+func _generate_random_position(subsector_coordinates: Vector2, sector_top_left: Vector2) -> Vector2:
 	# Find the top left and bottom right of the sub-sector +/- its padding
-	var minimum := (
+	var subsector_top_left := (
 		sector_top_left
-		+ Vector2(_sub_sector_base_size, _sub_sector_base_size) * sub_sector_coordinates
-		+ Vector2(_sub_sector_margin, _sub_sector_margin)
+		+ Vector2(_subsector_base_size, _subsector_base_size) * subsector_coordinates
+		+ Vector2(_subsector_margin, _subsector_margin)
 	)
-	var maximum := minimum + Vector2(_sub_sector_size, _sub_sector_size)
-	return Vector2(_rng.randf_range(minimum.x, maximum.x), _rng.randf_range(minimum.y, maximum.y))
+	var subsector_bottom_right := subsector_top_left + Vector2(_subsector_size, _subsector_size)
+	return Vector2(
+		_rng.randf_range(subsector_top_left.x, subsector_bottom_right.x),
+		_rng.randf_range(subsector_top_left.y, subsector_bottom_right.y)
+	)
